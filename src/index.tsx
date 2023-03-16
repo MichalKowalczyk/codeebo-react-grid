f9af3a914c7c989750a628b1b01e5ee4dd1cd9e3
Michał Kowalczyk <michal.p.kowalczyk@gmail.com>
2023-03-16 13:35:20 +0100

    fix imports

--- src/index.tsx
+++ src/index.tsx
@@ -4,3 +4,5 @@ export { default as Container } from "./components/container/container";
 export { default as Grid } from "./components/grid/grid";
 export { default as Row } from "./components/row/row";
 export { default as Col } from "./components/col/col";
+export { default as GenericList } from "./components/generic-list/generic-list";
+
